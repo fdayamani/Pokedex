401a44ee2de8d56c831fea2c1fa145eeb5508ade
Fatema Damani <fatema.damani@gmail.com>
2020-06-07 09:34:35 +0100

    Allows cors to be able to access this from the front end

--- src/main/java/com/example/pokedex/PokedexController.java
+++ src/main/java/com/example/pokedex/PokedexController.java
@@ -1,10 +1,12 @@
 package com.example.pokedex;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PokedexController {
+    @CrossOrigin
     @RequestMapping("/")
     public String pokedex() {
         return "Hello World";
